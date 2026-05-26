b7f46994a963c8420573ca49175735d43748da5f
ronfaho-rgb <ronfaho@gmail.com>
2026-05-26 10:45:18 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,15 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conexion = new mysqli(
-    "sql313.infinityfree.com",  // HOST
-    "if0_41477748",              // USUARIO
-    "PigbPP5Qgv8vi",              // PASSWORD
-    "if0_41477748_inventario"    // BASE DE DATOS
-);
+// Credenciales nuevas de tu base de datos en Railway
+$host     = "mysql.railway.internal";
+$usuario  = "root"; // En Railway el usuario por defecto casi siempre es root
+$password = "MJWluisAMAblEzNdnRRbXltbnPrRLlTx";
+$db_name  = "railway"; // En Railway la base de datos automática se llama railway
+$puerto   = 3306; // El puerto estándar de MySQL
+
+// Conexión incluyendo el parámetro del puerto al final para Railway
+$conexion = new mysqli($host, $usuario, $password, $db_name, $puerto);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-
-
+?>
